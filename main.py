906f303b83e1117bb6cfc190198bbbb6b0be4f58
Vladimir <volkvid@yandex.ru>
2014-11-19 00:20:17 +0300

    Update main.py
    CSV support, good parsing.
    
    next - add parsing lists for lists; add logic to mix both methods in one mega mech :)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests, wikipedia, sys
+import requests, wikipedia, sys, csv
 from bs4 import BeautifulSoup as bs
 
 endLists = []
@@ -7,24 +7,24 @@ def main():
     getReady()
     parseEndLists(endLists)
 
+# add list2list parse feature 
 
 def parseEndLists(List):
     headers = {"User-agent":"Mozilla/5.0"}
     endpoint = "http://en.wikipedia.org"
 
-    # recreate file
-    f = open("output/hrefs.txt", "w")
+    f = open("output/hrefs.txt", "w") # demo file
     f.close()
+
+    j = 0
     for link in List:
         if link != "": #for stabilyty
             r = requests.get(link, headers=headers)
             soup = bs(r.text)
             hrefs = soup.find_all("a")
-            print "\n", len(hrefs), "hrefs"
-
-            f = open("output/hrefs.txt", "a")
+            print "\n", len(hrefs), "hrefs\n"
             # --
-            j = 0
+            
             # --
             for hr in hrefs:
                 sthr = str(hr)                    
@@ -39,46 +39,95 @@ def parseEndLists(List):
                     if s1 > s0:
                         #the endpoint to Article page
                         sthr = endpoint + sthr[s0:s1] 
+                        f = open("output/hrefs.txt", "a")
                         f.write(sthr + "\n")
-
-                        parsePeople(sthr)
-
-                        j += 1
+                        f.close()
+                        if parsePeople(sthr):
+                            print j
+                            j += 1
                         
                     # -- testing --
-                    if j == 10:
-                        break
+                    #if j == 10:
+                        #break
                     # -- 
-
-            f.close()
-
+    print "total processed", j
+    
 
 def parsePeople(link):
     # load and parse link
     template = {}
     headers = {"User-agent":"Mozilla/5.0"}
-    r = requests.get(link, headers=headers)
-    soup = bs(r.text)
+    
+    try:
+        r = requests.get(link, headers=headers)
+        soup = bs(r.text)
 
-    allclasses = ["fn", "nickname", "bday", "dday deathdate", "role"]
-    for aclass in allclasses:
-        fn = soup.find_all("span", class_= aclass)
-        if len(fn) > 0:
-            item = fn[0]
-            f = item.contents
-            # check if this has no tags
-            if f[0].find("<") == -1 and f[0].find(">") == -1:
-                print aclass, str(f[0]) 
-                template[aclass] = f[0]
+        allclasses = ["fn", "nickname", "bday", "dday deathdate", "role"]
+        for aclass in allclasses:
+            if aclass == "role":
+                #<td class="role"> -- if role-key
+                fn = soup.find_all("td", class_ = aclass)
+            else:
+                fn = soup.find_all("span", class_= aclass)
+                
+            if len(fn) > 0:
+                for item in fn:
+                    f = item.contents
+                    if f[0].find("<") == -1 and f[0].find(">") == -1:
+                        template[aclass] = f[0]
+        # now template is fulled 
+        if len(template) > 1:
+            template["link"] = link
+            try:
+                ofile = open("output/output.csv", "a")
+                lis = generateCSVstring(template)
+                ofile.write(lis)
+                ofile.close()
+                return True
+            except:
+                print "Error in output file"
+                return False
+                #sys.exit(1)
+        
+    except Exception as ex:
+        print "(parse r)", str(ex)
+        return False
+                
 
-    if len(template) > 0:
-        template["link"] = link
-        print len(template)-1, "item(s):\n", template, "\n"
+def generateCSVstring(dic):
+    # prepare CSV row here
+    st = ""
+    br = "; "
+    try:
+        st += dic["fn"].encode("utf8")
+        st += br
+    except:
+        st += br
 
+    try:
+        st += dic["nickname"].encode("utf8")
+        st += br
+    except:
+        st += br
 
-    # prepare CSV template, don't save if empty
-    #"fullname":"", "nickname":"", "bday":"", "dday":"", "role":""
-    #print len(template), "full fields"
+    try:
+        st += dic["bday"].encode("utf8")
+        st += br
+    except:
+        st += br
+           
+    try:
+        st += dic["dday deathdate"].encode("utf8")
+        st += br
+    except:
+        st += br
+
+    try:
+        st += dic["link"].encode("utf8")
+    except:
+        pass
+
+    return st + "\n"
     
 
 def getReady():
@@ -93,6 +142,9 @@ def getReady():
     except:
         print "File 'lists.txt' not found"
         sys.exit("Create one")
+
+    out = open("output/output.csv", "w")
+    out.close()
 
 
 main()
