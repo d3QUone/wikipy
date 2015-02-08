bbfb367ad4346ca0020b5baafc8c67c0320e50b6
d3QUone <volkvid@yandex.ru>
2015-02-08 14:57:24 +0300

    new source-site

--- main.py
+++ main.py
@@ -248,10 +248,47 @@ def parseTables(page_link):
     except Exception as ex:
         print "(parseTables, r)", format_exception(ex)
         print "--"*25
-    return template 
+    return template
+
+
+def parseOtherSite(page_link):
+    template = []
+    append = template.append
+    headers = {"User-agent":"Mozilla/5.0"}
+    try:
+        r = requests.get(page_link, headers = headers, timeout = 10)
+        soup = bs(r.text)
+        all_lists = soup.find_all("li", class_="")
+        print "\n---found {0} list items on the page---\n".format(len(all_lists))
+        for item in all_lists:
+            buf = str(item)
+            sub_res = []
+            sub_pend = sub_res.append
+            
+            a = buf.find('href="')
+            buf = buf[a:]
+            a = buf.find('href="') + len('href="')
+            buf = buf[a:]
+            b = buf.find('"')
+            href = buf[:b]
+            buf = buf[b+2:]
+            sub_pend(href)
+            
+            while buf.find("<") != -1 and buf.find(">") != -1:
+                a = buf.find("<")
+                b = buf.find(">")
+                buf = buf[:a] + buf[b+1:]
+            sub_pend(buf)
+            
+            append(sub_res) # whole current table
+    except Exception as ex:
+        print "(parseTables, r)", format_exception(ex)
+        print "--"*25
+    return template
+    
 
            
-if __name__ == "__main__": 
+if __name__ == "__main__":
     file_set = "9"
     endpoint = "http://en.wikipedia.org/"
     links = {
@@ -260,13 +297,16 @@ if __name__ == "__main__":
         "8": ["http://en.wikipedia.org/wiki/Forbes%27_list_of_world%27s_highest-paid_athletes",
               "http://en.wikipedia.org/wiki/40_under_40_(Fortune_magazine)",
               "http://en.wikipedia.org/wiki/Forbes_list_of_The_World%27s_100_Most_Powerful_Women"],
-        "9": []
+        "9": ["http://www.biographyonline.net/people/famous-100.html"]
               }
 
     already_saved = []
     append = already_saved.append
     for link in links[file_set]:
-        res = parseTables(link)
+        #res = parseTables(link) # set < 9
+        res = parseOtherSite(link)
+        print res
+        '''
         print "--- Got a set of {0} tables".format(len(res))
         for item in res:
             print "- Got a set of {0} rows".format(len(item))
@@ -283,4 +323,10 @@ if __name__ == "__main__":
                         print "already saved"
                     i += 1
                 i += 1
+        '''
     print "\nDone...\n"
+
+
+
+
+    
