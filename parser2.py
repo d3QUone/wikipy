1245e261d1cbc877c5b2a7d05cf0952d596eca47
d3QUone <volkvid@yandex.ru>
2015-02-09 18:17:38 +0300

    new site (endpoint) added

--- parser2.py
+++ parser2.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import requests, sys, json, traceback
 from bs4 import BeautifulSoup as bs
 from datetime import datetime
@@ -24,68 +23,97 @@ def parseOtherSite(page_link):
     append = template.append
     headers = {"User-agent":"Mozilla/5.0"}
     try:
+        # ol -> li
         r = requests.get(page_link, headers = headers, timeout = 10)
         soup = bs(r.text)
-        all_lists = soup.find_all("li", class_="")
-        print "\n---found {0} list items on the page---\n".format(len(all_lists))
-        for item in all_lists:
-            buf = str(item)
-            sub_res = []
-            sub_pend = sub_res.append
-            
-            a = buf.find('href="')
-            buf = buf[a:]
-            a = buf.find('href="') + len('href="')
-            buf = buf[a:]
-            b = buf.find('"')
-            href = buf[:b]
-            buf = buf[b+2:]
-            sub_pend(href)
-            
-            while buf.find("<") != -1 and buf.find(">") != -1:
-                a = buf.find("<")
-                b = buf.find(">")
-                buf = buf[:a] + buf[b+1:]
-            sub_pend(buf)
-            
-            append(sub_res) # whole current table
+        find_ol = soup.find_all("ol")
+        print "Current page has {0} <ol>-tags".format(len(find_ol))
+        for ol_tag in find_ol:
+            sub_soup = bs(str(ol_tag))
+            find_li = soup.find_all("li", class_="")
+            print "Current page has {0} <li>-tags".format(len(find_li))
+            for li_tag in find_li:
+                buf = str(li_tag)
+                sub_res = []
+                sub_pend = sub_res.append
+                
+                a = buf.find('href="')
+                buf = buf[a:]
+                a = buf.find('href="') + len('href="')
+                buf = buf[a:]
+                b = buf.find('"')
+                href = buf[:b]
+                buf = buf[b+2:]
+                sub_pend(href)
+                
+                while buf.find("<") != -1 and buf.find(">") != -1:
+                    a = buf.find("<")
+                    b = buf.find(">")
+                    buf = buf[:a] + buf[b+1:]
+                sub_pend(buf)
+                append(sub_res) # whole current table
     except Exception as ex:
         print "(parseOtherSite, r)", format_exception(ex)
         print "--"*25
     return template
-    
 
+
+numerals = [str(i) for i in xrange(10)] # need for detecting dates
            
 if __name__ == "__main__":
-    file_set = "9" 
+    file_set = "9"
+    main_domain = "http://www.biographyonline.net"
+    print "Found {0} links on {1} file-set...".format(len(links[file_set]), file_set)
 
     already_saved = []
     append = already_saved.append
     for link in links[file_set]:
+        j = 0 # stats
         res = parseOtherSite(link)
-        print res
-        
-        '''
-        print "--- Got a set of {0} tables".format(len(res))
+        print ""
         for item in res:
-            print "- Got a set of {0} rows".format(len(item))
-            i = 0
-            while i < len(item):
-                row = item[i]
-                personalData = parsePeople(endpoint + row)
-                if personalData:
-                    personalData["role"] = item[i + 1]
-                    if personalData not in already_saved:
-                        append(personalData)
-                        do_saving(personalData, file_set)
-                    else:
-                        print "already saved"
-                    i += 1
-                i += 1
-        '''
-    print "\nDone...\n"
+            # 1 item == 1 man: item[0] - link, item[1] - data....
+            link = item[0]
+            a = link.find("/")
+            link = link[a:]
+            if len(link) > 0:
+                link = main_domain + link
+                data = {}
+                data["link"] = link
 
+                p_data = item[1] # name ... ( b day .... d day ) ... role
+                a = p_data.find("(")
+                if a != -1:
+                    fn = p_data[:a]
+                    data["fn"] = fn
+                    p_data = p_data[a+1:]
+                    
+                    a = p_data.find(")")
+                    dates = p_data[:a]
+                    data["role"] = p_data[a+1:]
 
+                    bday = ""; dday = ""
+                    i = 0; f = 0
+                    while i < len(dates):
+                        char = dates[i]
+                        if char in numerals and f == 0:
+                            bday += char
+                            f = 1
+                        elif char in numerals and f == 1:
+                            bday += char
+                        elif char in numerals and f == 2:
+                            dday += char
+                        else:
+                            f = 2
+                        i += 1
+                    data["bday"] = bday
+                    data["dday deathdate"] = dday
 
-
+                    # save it :)
+                    if data not in already_saved:
+                        append(data)
+                        do_saving(data, file_set)
+                        j += 1
+        print "\n\nactually datas: {0}\n".format(j)
+    print "\nDone..."
     
--- saving.py
+++ saving.py
@@ -1,36 +1,67 @@
 # prepare a table row for .CSV here
-def generateCSVstring(dic):
+def generateCSVstring(dic, utf8 = False):
     st = ""; br = "; "; em = " "
-    try:
-        st += dic["fn"].encode("utf8") + br
-    except:
-        st += em + br
-    try:
-        st += dic["nickname"].encode("utf8") + br
-    except:
-        st += em + br
-    try:
-        if isinstance(dic["role"], list):
-            for role in dic["role"]:
-                st += role.encode("utf8") + ", "
-            st += br
-        else:
-            st += dic["role"].encode("utf8") + br
-    except:
-        st += em + br
-    try:
-        st += dic["bday"].encode("utf8") + br
-    except:
-        st += em + br
-    try:
-        st += dic["dday deathdate"].encode("utf8") + br
-    except:
-        st += em + br
-    try:
-        st += dic["link"].encode("utf8")
-    except:
-        st += em
-        pass
+    if utf8:
+        try:
+            st += dic["fn"].encode("utf8") + br
+        except:
+            st += em + br
+        try:
+            st += dic["nickname"].encode("utf8") + br
+        except:
+            st += em + br
+        try:
+            if isinstance(dic["role"], list):
+                for role in dic["role"]:
+                    st += role.encode("utf8") + ", "
+                st += br
+            else:
+                #print "encode:", dic["role"].encode("utf8"), "\n"
+                st += dic["role"].encode("utf8") + br
+        except:
+            st += em + br
+        try:
+            st += dic["bday"].encode("utf8") + br
+        except:
+            st += em + br
+        try:
+            st += dic["dday deathdate"].encode("utf8") + br
+        except:
+            st += em + br
+        try:
+            st += dic["link"].encode("utf8")
+        except:
+            st += em
+    else:    
+        try:
+            st += dic["fn"] + br
+        except:
+            st += em + br
+        try:
+            st += dic["nickname"] + br
+        except:
+            st += em + br
+        try:
+            if isinstance(dic["role"], list):
+                for role in dic["role"]:
+                    st += role + ", "
+                st += br
+            else:
+                st += dic["role"] + br
+        except:
+            st += em + br
+        try:
+            st += dic["bday"] + br
+        except:
+            st += em + br
+        try:
+            st += dic["dday deathdate"] + br
+        except:
+            st += em + br
+        try:
+            st += dic["link"]
+        except:
+            st += em
     return st + "\n"
 
 
@@ -40,7 +71,7 @@ def do_saving(dict_to_save, file_set, log = False):
         ofile = open("output/output{0}.csv".format(file_set), "a")
     except: 
         ofile = open("output/output{0}.csv".format(file_set), "w")
-    lis = generateCSVstring(dict_to_save)
+    lis = generateCSVstring(dict_to_save, utf8 = False)
     ofile.write(lis)
     ofile.close()
     if log:
