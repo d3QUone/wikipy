47b6cdf850d463eb8754f83fc3219f7216ae74a1
d3QUone <volkvid@yandex.ru>
2015-02-11 19:45:19 +0300

    fixed back

--- saving.py
+++ saving.py
@@ -1,67 +1,36 @@
 # prepare a table row for .CSV here
 def generateCSVstring(dic, utf8 = False):
     st = ""; br = "; "; em = " "
-    if utf8:
-        try:
-            st += dic["fn"].encode("utf8") + br
-        except:
-            st += em + br
-        try:
-            st += dic["nickname"].encode("utf8") + br
-        except:
-            st += em + br
-        try:
-            if isinstance(dic["role"], list):
-                for role in dic["role"]:
-                    st += role.encode("utf8") + ", "
-                st += br
-            else:
-                #print "encode:", dic["role"].encode("utf8"), "\n"
-                st += dic["role"].encode("utf8") + br
-        except:
-            st += em + br
-        try:
-            st += dic["bday"].encode("utf8") + br
-        except:
-            st += em + br
-        try:
-            st += dic["dday deathdate"].encode("utf8") + br
-        except:
-            st += em + br
-        try:
-            st += dic["link"].encode("utf8")
-        except:
-            st += em
-    else:    
-        try:
-            st += dic["fn"] + br
-        except:
-            st += em + br
-        try:
-            st += dic["nickname"] + br
-        except:
-            st += em + br
-        try:
-            if isinstance(dic["role"], list):
-                for role in dic["role"]:
-                    st += role + ", "
-                st += br
-            else:
-                st += dic["role"] + br
-        except:
-            st += em + br
-        try:
-            st += dic["bday"] + br
-        except:
-            st += em + br
-        try:
-            st += dic["dday deathdate"] + br
-        except:
-            st += em + br
-        try:
-            st += dic["link"]
-        except:
-            st += em
+    try:
+        st += dic["fn"].encode("utf8") + br
+    except:
+        st += em + br
+    try:
+        st += dic["nickname"].encode("utf8") + br
+    except:
+        st += em + br
+    try:
+        if isinstance(dic["role"], list):
+            for role in dic["role"]:
+                st += role.encode("utf8") + ", "
+            st += br
+        else:
+            #print "encode:", dic["role"].encode("utf8"), "\n"
+            st += dic["role"].encode("utf8") + br
+    except:
+        st += em + br
+    try:
+        st += dic["bday"].encode("utf8") + br
+    except:
+        st += em + br
+    try:
+        st += dic["dday deathdate"].encode("utf8") + br
+    except:
+        st += em + br
+    try:
+        st += dic["link"].encode("utf8")
+    except:
+        st += em
     return st + "\n"
 
 
